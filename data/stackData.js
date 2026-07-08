7e31f4b010712dc3129d5508c1435bbd9f15e9aa
riverimenemessadh <sarahimenemessadh@gmail.com>
2026-07-08 22:46:19 +0100

    Update stack technologies

--- data/stackData.js
+++ data/stackData.js
@@ -1,18 +1,19 @@
 export const stack = {
   web: [
     { name: 'PHP', icon: '/assets/icons/php.svg' },
-    { name: 'Vite', icon: '/assets/icons/vite.svg' },
     { name: 'Laravel', icon: '/assets/icons/laravel.svg' },
-    { name: 'HTML', icon: '/assets/icons/html5.svg' },
-    { name: 'CSS', icon: '/assets/icons/css3.svg' },
-    { name: 'JavaScript', icon: '/assets/icons/javascript.svg' },
-    {name: 'TypeScript', icon: '/assets/icons/typescript.svg'},
     { name: 'React', icon: '/assets/icons/react.svg' },
     { name: 'Next.js', icon: '/assets/icons/nextjs.svg' },
-    { name: 'MySQL', icon: '/assets/icons/mysql.svg' },
+    { name: 'Vite', icon: '/assets/icons/vite.svg' },
+    { name: 'HTML', icon: '/assets/icons/html5.svg' },
+    { name: 'CSS', icon: '/assets/icons/css3.svg' },
     { name: 'Tailwind CSS', icon: '/assets/icons/tailwindcss.svg' },
-    { name: 'WordPress', icon: '/assets/icons/wordpress.svg' },
+    { name: 'JavaScript', icon: '/assets/icons/javascript.svg' },
+    {name: 'TypeScript', icon: '/assets/icons/typescript.svg'},
     { name: 'Java', icon: '/assets/icons/java.svg' },
+    { name: 'MySQL', icon: '/assets/icons/mysql.svg' },
+    { name: 'WordPress', icon: '/assets/icons/wordpress.svg' },
+    
   ],
   mobile: [
     { name: 'Flutter', icon: '/assets/icons/flutter.svg' },
